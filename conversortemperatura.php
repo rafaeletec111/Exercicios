d585ad4bd7f73d8255b05e7266294165389ece60
Rafael <rafaelmassaki242@gmail.com>
2024-08-29 22:14:23 -0300

    first commit

---
+++ conversortemperatura.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $temperaturaCelsius = 30;
+    $fahrenheit = ($temperaturaCelsius * 9/5) + 32;
+    echo "os graus e tfahrenheit é $fahrenheit";
+    ?>
+</body>
+</html>
